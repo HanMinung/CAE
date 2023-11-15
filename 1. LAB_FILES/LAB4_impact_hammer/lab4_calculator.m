444d1cb92034cfa45d254e5d1efdbb0c22043a03
HanMinung <99113269+HanMinung@users.noreply.github.com>
2023-11-15 14:14:52 +0900

    CAE lab file added

---
+++ 1. LAB_FILES/LAB4_impact_hammer/lab4_calculator.m
@@ -0,0 +1,70 @@
+clear;
+
+al_cant_fft = readmatrix('./exp_data/aluminium_cantilever_fft.txt');
+st_cant_fft = readmatrix('./exp_data/steel_cantilever_fft.txt');
+br_cant_fft = readmatrix('./exp_data/brass_cantilever_fft.txt');
+st_fix_fft  = readmatrix('./exp_data/steel_fixedsupport_fft.txt');
+
+freq = al_cant_fft(8:end-1, 1);
+
+figure;
+plot(freq, al_cant_fft(8:end-1, 2), 'r-',lineWidth = 3);
+grid minor;     xlabel("frequency [Hz]");       ylabel("magnitude [-]");
+title("FFT result of aluminum with cantilever beam condition");     legend("Al / cantilever", fontSize = 12);
+
+figure;
+plot(freq, st_cant_fft(8:end-1, 2), 'r-',lineWidth = 3);
+grid minor;     xlabel("frequency [Hz]");       ylabel("magnitude [-]");
+title("FFT result of steel with cantilever beam condition");     legend("st / cantilever", fontSize = 12);
+
+figure;
+plot(freq, br_cant_fft(8:end-1, 2), 'r-',lineWidth = 3);
+grid minor;     xlabel("frequency [Hz]");       ylabel("magnitude [-]");
+title("FFT result of brass with cantilever beam condition");     legend("br / cantilever", fontSize = 12);
+
+figure;
+plot(freq, st_fix_fft(8:end-1, 2), 'r-',lineWidth = 3);
+grid minor;     xlabel("frequency [Hz]");       ylabel("magnitude [-]");
+title("FFT result of steel with fixed boundary condition");     legend("st / fixed", fontSize = 12);
+
+%% Theoretical value
+clear;
+
+cant_w     =  19e-3;     % [m]
+cant_t     =  3e-3;      % [m]
+cant_l     =  600e-3;    % [m]
+
+fix_w      =  19e-3;     % [m]
+fix_t      =  3e-3;      % [m]
+fix_l      =  600e-3;    % [m]
+
+beta_cant = [1.875104, 4.694091, 7.854757, 10.995541]./cant_l;
+beta_fix  = [4.720041, 7.853205, 10.995608, 14.137165]./fix_l;
+
+% steel - brass - aluminum sequence
+density   = [7850  , 8740  , 2700];    % [kg/m^3] 
+E         = [200e9 , 105e9 , 70e9];    % Pa
+I         = cant_w * cant_t^3/12;      % [m^4]
+A         = cant_w * cant_t;           % [m^2]
+
+% steel / cantilver condition
+for Idx = 1 : 4
+
+    % steel / cantilever
+    omega_st_can(Idx) = sqrt((beta_cant(Idx)^4*E(1)*I)/(density(1)*A));
+    freq_st_can(Idx)  = omega_st_can(Idx)/(2*pi);
+
+    % brass
+    omega_br(Idx) = sqrt((beta_cant(Idx)^4*E(2)*I)/(density(2)*A));
+    freq_br(Idx)  = omega_br(Idx)/(2*pi);
+
+    % aluminum
+    omega_al(Idx) = sqrt((beta_cant(Idx)^4*E(3)*I)/(density(3)*A));
+    freq_al(Idx)  = omega_al(Idx)/(2*pi);
+
+    % steel / fixed
+    ometa_st_fix(Idx) = sqrt((beta_fix(Idx)^4*E(1)*I)/(density(1)*A));
+    freq_st_fix(Idx)  = ometa_st_fix(Idx)/(2*pi);
+
+end
+
---
+++ README.md
@@ -0,0 +1,19 @@
+# CAE mechanical design and analysis 
+
+@ Handong global university
+
+@ Mechanical and control engineering
+
+@ subject : finite element method using ANSYS
+
+@ semester : 2023 fall semester
+
+@ author : Han Minwoong
+
+
+
+​			This repository introduces the process and results of structural analysis of various machine shapes using finite element analysis. The tool used for finite element analysis is ansys (2023 version). I mainly analyzed the maximum stress and degree of deformation of the stress acting on machine elements. In addition, it deals with the fatigue analysis of the Goodman diagram and mainly deals with simulation of the resonance frequency measurement of the system based on the modal analysis method in simulation program. In addition, it contains experiments on strain gauge, combined stress, resonance frequency of multi story building, impact hammer test, etc. The experiments mainly involved comparing theoretical values, experimental values, and simulation values, and all relevant reports are included. Lastly, the final project deals with structural analysis issues regarding the maximum stress on the body frame and deflection of the motor connection of a self-made electric vehicle. Furthermore, we will look for improvements to the motor's fixed points, perform structural analysis on the improved model, and evaluate its performance.
+
+
+
+## Contents
